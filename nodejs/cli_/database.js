0547807c936e14c72c2efa205a01cf65e0048fdb
Cauã Gomes Xavier <114945046+Cauaxavier@users.noreply.github.com>
2023-07-01 17:30:08 -0300

    manipulando arquivos(create).

--- nodejs/cli_/database.js
+++ nodejs/cli_/database.js
@@ -1,24 +1,45 @@
-const { readFile } = require('fs');
-const { promisify } = require('util');
+const { readFile, writeFile } = require("fs");
+const { promisify } = require("util");
 
-const readFileAsync = promisify(readFile)
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
+// other form to get the json's data
+// const dataJson = require('/.heroes.json')
 
 class Database {
-    constructor() {
-        this.name_file = 'heroes.json'
-    }
-    async getDataFile() {
-        const file = await readFileAsync(this.name_file, 'utf-8');
-        return JSON.parse(file.toString());
-    }
-    writeFile() {
+  constructor() {
+    this.name_file = "heroes.json";
+  }
 
-    }
-    async list(id) {
-        const data = await this.getDataFile();
-        const filtered_data = data.filter(item => (id ? (item.id === id) : true))
-        return filtered_data
-    }
+  async getDataFile() {
+    const file = await readFileAsync(this.name_file, "utf-8");
+    return JSON.parse(file.toString());
+  }
+
+  async write_file(data) {
+    await writeFileAsync(this.name_file, JSON.stringify(data));
+    return true;
+  }
+
+  async register(hero) {
+    const data = await this.getDataFile();
+    const id = hero.id <= 2 ? hero.id : Date.now();
+
+    const heroWithId = {
+      id,
+      ...hero,
+    };
+    const finalData = [...data, heroWithId];
+
+    const result = await this.write_file(finalData);
+    return result;
+  }
+
+  async list(id) {
+    const data = await this.getDataFile();
+    const filtered_data = data.filter((item) => (id ? item.id === id : true));
+    return filtered_data;
+  }
 }
 
-module.exports = new Database()
+module.exports = new Database();
--- nodejs/cli_/test.js
+++ nodejs/cli_/test.js
@@ -9,6 +9,10 @@ const default_item_register = {
 };
 
 describe("heroes manipulation suite", () => {
+  before(async () => {
+    await database.register(default_item_register);
+  });
+
   it("must search a hero using the files", async () => {
     const expected = default_item_register;
     const [result] = await database.list(expected.id);
@@ -16,13 +20,13 @@ describe("heroes manipulation suite", () => {
     deepEqual(result, expected);
   });
 
-  /*    
   it("Must register a hero, using files", async () => {
-    const expected = {};
+    const expected = default_item_register;
+    const result = await database.register(default_item_register);
+    const [actual] = await database.list(default_item_register.id);
 
-    ok(null, expected);
+    deepEqual(actual, expected);
   });
-  */
 });
 
 // for execute programme use: node node_modules/mocha/bin/mocha tests.js or npm test
